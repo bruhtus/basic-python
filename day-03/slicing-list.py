9bcd673577ed8a5c02a1f3a6ddd7558733ce9605
bruhtus <hibruhtus@gmail.com>
2020-10-18 18:25:01 +0700

    Day 3 finished

--- day-03/slicing-list.py
+++ day-03/slicing-list.py
@@ -6,6 +6,6 @@ print(f'orange and mango: {fruits[1:3]}')
 
 print(f'orange, mango, lemon: {fruits[1:]}')
 
-print(f'print every 2 step: {fruits[::-2]}\n')
+print(f'print every 2 step: {fruits[::-2]}\n') #from behind
 
 print(f"does lime exist in fruits list? {'lime' in fruits}")
